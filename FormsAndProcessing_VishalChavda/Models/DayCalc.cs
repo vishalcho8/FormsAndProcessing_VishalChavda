9b13040a1988978993b26421bfd7c41bcd7a3ee7
vishal <vishalchavdaaugst@gmail.com>
2021-12-12 17:34:43 -0500

    Add project files.

---
+++ FormsAndProcessing_VishalChavda/Models/DayCalc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FormsAndProcessing_VishalChavda.Models
+{
+    public class DayCalc
+    {
+        [Display(Name = "Select Date"), DataType(DataType.Date), DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime Date { get; set; }
+
+        [Display(Name = "Add Days")]
+        public int Day { get; set; }
+
+        [Display(Name = "New Date (Result)")]
+        public String NewDate { get; set; }
+    }
+}
---
+++ FormsAndProcessing_VishalChavda/Pages/Index.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FormsAndProcessing_VishalChavda.Models;
+
+namespace FormsAndProcessing_VishalChavda.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        [FromForm]
+        public DayCalc DayCalc { get; set; }
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+             
+        }
+
+        public void OnPost()
+        {
+           var x = DayCalc.Date.AddDays(DayCalc.Day);
+           DayCalc.NewDate = x.ToString("yyyy-MM-dd");
+
+
+        }
+    }
+}
